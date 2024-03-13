969ef4b036e591f0b4c3b7950457bbec660ee067
Chinmai Kolthur <Chinmai.Kolthur@healthonus.io>
2024-03-13 13:08:40 +0530

    -Added Character & RpgClass Classes, -Added CharacterController & Get a New Character Api - First steps with routing    -- Added GetAll Characters Api & Get first character from list , using routing - Routing with parameters    -- Modified GetSingle api method , to get character based on given id, used parameter in routing

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using Dotnet_Rpg_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Rpg_Project.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> 
+        {
+            new Character(),
+            new Character {Id = 2, Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if (character is not null)
+            {
+                return Ok(character);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dotnet_Rpg_Project.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
