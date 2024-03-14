30585660ba63b2dc6310883ec4e912fe2efcae92
Chinmai Kolthur <Chinmai.Kolthur@healthonus.io>
2024-03-14 16:51:11 +0530

    Installed and Configured AutoMapper Made changes in CharacterService class accordingly

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Dotnet_Rpg_Project.DTO.Character;
+using Dotnet_Rpg_Project.Models;
+
+namespace Dotnet_Rpg_Project
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character,GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,4 +1,5 @@
-﻿using Dotnet_Rpg_Project.DTO.Character;
+﻿using AutoMapper;
+using Dotnet_Rpg_Project.DTO.Character;
 using Dotnet_Rpg_Project.Models;
 
 namespace Dotnet_Rpg_Project.Services.CharacterService
@@ -10,18 +11,26 @@ namespace Dotnet_Rpg_Project.Services.CharacterService
             new Character(),
             new Character {Id = 2, Name = "Sam"}
         };
+        private readonly IMapper _mapper;
+
+        public CharacterService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(newCharacter);
-            serviceResponse.Data = characters;
+            var character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character);
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters;
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
 
@@ -29,7 +38,7 @@ namespace Dotnet_Rpg_Project.Services.CharacterService
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
             var character =  characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = character;
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             return serviceResponse;
         }
     }
