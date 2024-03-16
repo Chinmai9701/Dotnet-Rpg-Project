9aa7ac79a4f018fd931a75d9d404efd9fef0086e
Chinmai Kolthur <Chinmai.Kolthur@healthonus.io>
2024-03-16 11:23:46 +0530

    -Installed microsoft entityframecore & entityframeworkcore.sqlserver from nuget package manager -Installed SSMS -Added the DataContext -Added the ConnectionString and DbContext configuration

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Dotnet_Rpg_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dotnet_Rpg_Project.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base((DbContextOptions) options)
+        {
+
+        }
+        public DbSet<Character> characters => Set<Character>();
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Dotnet_Rpg_Project.Data;
 using Dotnet_Rpg_Project.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
